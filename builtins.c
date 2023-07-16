4078844d7e0a91a2c652ab1ab4eb5d3fc3bba0aa
Joao Marcos <joao.marcos.witcel@gmail.com>
2023-07-16 00:02:18 -0300

    const

--- builtins.c
+++ builtins.c
@@ -6,13 +6,13 @@ int builtin_cd(char **args);
 int builtin_help(char **args);
 int builtin_exit(char **args);
 
-char *builtin_cstring[] = {
+const char *builtin_cstring[] = {
   "cd",
   "help",
   "exit",
 };
 
-Builtin_Function builtin_func[] = {
+const Builtin_Function builtin_func[] = {
   &builtin_cd,
   &builtin_help,
   &builtin_exit,
