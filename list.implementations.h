32895e4bbe9ea3c4bb527839059857301f9eea70
Joao Marcos <joao.marcos.witcel@gmail.com>
2023-08-03 13:02:46 -0300

    ajustando

--- list.implementations.h
+++ list.implementations.h
@@ -1,3 +1,9 @@
+#ifndef LIST_IMPLEMENTATIONS_H
+#define LIST_IMPLEMENTATIONS_H
+
+#include "./list.macro.h"
 
 MAKE_LIST_IMPLEMENTATION(List_Of_Strings, list_of_strings, const char *)
 MAKE_LIST_IMPLEMENTATION(List_Of_Floats, list_of_floats, float)
+
+#endif // LIST_IMPLEMENTATIONS_H
