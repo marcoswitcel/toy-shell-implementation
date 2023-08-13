b2ac076fe3c1bdf21b5673ddb14d8a6a79708264
Joao Marcos <joao.marcos.witcel@gmail.com>
2023-08-12 22:26:06 -0300

    mais macros

--- utils.macro.h
+++ utils.macro.h
@@ -4,5 +4,6 @@
 #define STRINGIFY(VALUE) #VALUE
 
 #define SIZE_OF_ARRAY(array) (sizeof(array) / sizeof(array[0]))
+#define SIZE_OF_STATIC_STRING(STRING) (SIZE_OF_ARRAY(STRING) - 1)
 
 #endif // UTILS_MACRO_H
