7c08599fa47a5f1a0e6d36b3a7be30e4243288c0
Joao Marcos <joao.marcos.witcel@gmail.com>
2023-09-25 00:38:36 -0300

    ajustando libs faltantes

--- src/dev-utils.c
+++ src/dev-utils.c
@@ -1,6 +1,8 @@
 #ifndef DEV_UTILS_C
 #define DEV_UTILS_C
 
+#include <stdio.h>
+#include <time.h>
 
 #define RECORD_TIME(IDENTIFIER) const clock_t IDENTIFIER = clock(); 
 #define MEASURE_TIME(IDENTIFIER, MESSAGE) printf("%s :: %s\ntempo em segundos %f\n", #IDENTIFIER, MESSAGE, ((double) (clock() - IDENTIFIER))/CLOCKS_PER_SEC);
