fbf91f4c5549a9b4cbc976bf1fb30899793aa69c
Joao Marcos <joao.marcos.witcel@gmail.com>
2023-11-02 00:42:20 -0300

    esboço dos testes

--- tests/sorting.test.c
+++ tests/sorting.test.c
@@ -1,7 +1,27 @@
 #include <assert.h>
 #include <string.h>
 
+#include "../src/sorting.c"
+#include "../src/utils.macro.h"
+#include "string.h"
+
+void test_lista_pequena()
+{
+  const char *arquivos[] = {
+    "casa",
+    "abacaxi",
+    "uva",
+  };
+
+  assert(strcmp(arquivos[0], "casa") == 0);
+  quick_sort_list(arquivos, 0, SIZE_OF_ARRAY(arquivos) - 1);
+  assert(strcmp(arquivos[0], "abacaxi") == 0);
+}
+
 extern void test_suit_sorting()
 {
-  // @todo João, implementar 
+  // @todo João, implementar
+
+  // @todo João, não passando
+  // test_lista_pequena();
 }
