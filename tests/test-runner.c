9770a60295fb00aff9611f13c7a96daa26857d3a
Joao Marcos <joao.marcos.witcel@gmail.com>
2023-12-20 22:45:58 -0300

    ajustando para usar buffer e write

--- tests/test-runner.c
+++ tests/test-runner.c
@@ -3,9 +3,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
 #include "./test-runner.h"
+#include "../src/buffer.h"
 #include "../src/list.macro.h"
+#include "../src/utils.c"
+#include "../src/utils.macro.h"
 #include "../src/list.implementations.h"
 #include "../src/ansi-escape-sequences.h"
 
@@ -54,10 +59,29 @@ void register_test(Test_Proc test, const char *name)
   list_of_strings_push(proc_names, name);
 }
 
+static void write_test_index(Buffer *buffer, unsigned reserved_space, unsigned current_index)
+{
+  char *test_number = int_to_cstring(current_index);
+    
+  for (unsigned i = 0; i < reserved_space - strlen(test_number); i++)
+  {
+    buffer_push(buffer, '0');
+  }
+
+  buffer_push_all(buffer, test_number, strlen(test_number));
+
+  free(test_number);
+}
+
 void test_runner(void)
 {
   ensure_is_initialized();
-  
+
+  Buffer *buffer = create_buffer(1024, 1024);
+  // @leak
+  unsigned index_size_in_chars = strlen(int_to_cstring(number_of_tests));
+  const int column_size = 80 - 1 - SIZE_OF_STATIC_STRING(" FAILED");
+
   for (unsigned i = 0; i < tests->index; i++)
   {
     // @todo João, resetar estrutura com contador de asserts e erros 
@@ -71,25 +95,60 @@ void test_runner(void)
     Test_Proc test = tests->data[i];
     test();
 
+    write_test_index(buffer, index_size_in_chars, i + 1);
+
+    buffer_push_all(buffer, EXPAND_STRING_REF_AND_COUNT(" "));
+
     // @todo João, pendências
     // - número de testes com zero a esquerda (fazer o max dos testes registrados para saber quantos zeros)
     // - limitar o número de caracteres por linha
     // - em caso de erro reportar dados úteis (line number, filename, etc...)
     // - usar um print apenas e um buffer para montar a linha
     const char *name = proc_names->data[i];
-    // @note por hora estou usando padding do C 
-    // @reference https://stackoverflow.com/questions/276827/string-padding-in-c
+    
+    buffer_push_all(buffer, name, strlen(name));
+
+    // @todo João, possível loop infinito(até o limite numérico do inteiro) por causa de underflow
+    for (unsigned i = 0; i < (column_size - strlen(name) - index_size_in_chars - 1); i++)
+    {
+      buffer_push(buffer, '.');
+    }
+
     if (current_state.at_least_one_failed)
     {
-      printf("%04d %-69s %sFAILED%s\n", i + 1, name, RED, RESET);
-      printf("     %s:%d Assertion: '%s'\n", current_state.filename, current_state.line_number, current_state.expr);
       number_of_failed_tests += 1;
+      buffer_push_all(buffer, EXPAND_STRING_REF_AND_COUNT(RED " FAILED" RESET));
     }
     else
     {
       number_of_success_tests += 1;
-      printf("%04d %-69s %sOK%s\n", i + 1, name, GREEN, RESET);
-    } 
+      buffer_push_all(buffer, EXPAND_STRING_REF_AND_COUNT(GREEN " OK" RESET));
+    }
+    buffer_push(buffer, '\n');
+
+    if (current_state.at_least_one_failed)
+    {
+      for (unsigned i = 0; i < index_size_in_chars + 1; i++)
+      {
+        buffer_push(buffer, ' ');
+      }
+
+      buffer_push_all(buffer, current_state.filename, strlen(current_state.filename));
+      buffer_push(buffer, ':');
+
+      char *line = int_to_cstring(current_state.line_number);
+      buffer_push_all(buffer, line, strlen(line));
+      free(line);
+      buffer_push_all(buffer, EXPAND_STRING_REF_AND_COUNT(" Assertion: '"));
+      buffer_push_all(buffer, current_state.expr, strlen(current_state.expr));
+      buffer_push(buffer, '\'');
+      buffer_push(buffer, '\n');
+    }
+
+
+    write(STDOUT_FILENO, buffer->buffer, buffer->index);
+
+    buffer_clear(buffer);
   }
 
   // resumo
