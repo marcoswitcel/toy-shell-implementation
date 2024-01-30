b60619b7fa8ffcbaaf2eea412ea9eac2ba92def4
Joao Marcos <joao.marcos.witcel@gmail.com>
2024-01-29 23:58:28 -0300

    mais testes

--- tests/parser.test.c
+++ tests/parser.test.c
@@ -1163,6 +1163,31 @@ void test_parse_execute_command_node_12(void)
   Assert(!node.append_mode_stderr);
 }
 
+void test_parse_execute_command_node_13(void)
+{
+  const char parse_input_sample[] = "echo teste 1> teste.txt | grep teste";
+
+  Parse_Context context = create_parse_context(parse_input_sample);
+  Sequence_Of_Tokens *tokens = tokenize(&context);
+
+  Execute_Command_Node node = parse_execute_command_node(&context, tokens);
+
+  Assert_Is_Not_Null(context.error);
+  //Assert(context.error_start_index == 13);
+
+  Assert_Is_Not_Null(node.args);
+  Assert_Is_Not_Null(node.args[0]);
+  Assert_Is_Not_Null(node.args[1]);
+  Assert_Is_Null(node.args[2]);
+
+  Assert_Is_Null(node.next_command);
+  Assert_Is_Null(node.pipe); // Nullo por que deu erro?
+  Assert_String_Equals(node.stdout_redirect_filename, "teste.txt");
+  Assert_Is_Null(node.stderr_redirect_filename);
+  Assert(!node.append_mode_stdout);
+  Assert(!node.append_mode_stderr);
+}
+
 extern void test_suit_parser(void)
 {
   Register_Test(test_create_parse_context);
@@ -1221,4 +1246,5 @@ extern void test_suit_parser(void)
   Register_Test(test_parse_execute_command_node_10);
   Register_Test(test_parse_execute_command_node_11);
   Register_Test(test_parse_execute_command_node_12);
+  Register_Test(test_parse_execute_command_node_13);
 }
