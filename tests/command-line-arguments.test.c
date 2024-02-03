b39a286325a7a42ceaef6ac19a56024533069083
Joao Marcos <joao.marcos.witcel@gmail.com>
2024-02-02 23:44:05 -0300

    mais testes

--- tests/command-line-arguments.test.c
+++ tests/command-line-arguments.test.c
@@ -47,9 +47,26 @@ void test_command_line_arguments_apply_argv03()
   Assert_Is_True(arguments.no_sound);
 }
 
+void test_command_line_arguments_apply_argv04()
+{
+  const char *argv[] = {
+    "--no_sound",
+    "--help",
+  };
+  int argc = SIZE_OF_ARRAY(argv);
+
+  Command_Line_Arguments arguments = { 0 };
+
+  command_line_arguments_apply_argv(&arguments, argc, argv);
+  Assert_Is_True(arguments.no_sound);
+  Assert_Is_True(arguments.help);
+  Assert_Is_False(arguments.colorful);
+}
+
 extern void test_suit_test_command_line_arguments()
 {
   Register_Test(test_command_line_arguments_apply_argv01);
   Register_Test(test_command_line_arguments_apply_argv02);
   Register_Test(test_command_line_arguments_apply_argv03);
+  Register_Test(test_command_line_arguments_apply_argv04);
 }
