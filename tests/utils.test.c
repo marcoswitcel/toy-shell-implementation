083f137951d6fa3c0801b694a84d308609f32667
Joao Marcos <joao.marcos.witcel@gmail.com>
2024-02-03 23:42:04 -0300

    .

--- tests/utils.test.c
+++ tests/utils.test.c
@@ -8,102 +8,102 @@ void test_int_to_cstring(void)
   const char *representation;
   
   representation = int_to_cstring(0);
-  Assert(strcmp(representation, "0") == 0);
+  Assert_String_Equals(representation, "0");
 
   representation = int_to_cstring(1);
-  Assert(strcmp(representation, "1") == 0);
+  Assert_String_Equals(representation, "1");
 
   representation = int_to_cstring(2);
-  Assert(strcmp(representation, "2") == 0);
+  Assert_String_Equals(representation, "2");
 
   representation = int_to_cstring(3);
-  Assert(strcmp(representation, "3") == 0);
+  Assert_String_Equals(representation, "3");
 
   representation = int_to_cstring(4);
-  Assert(strcmp(representation, "4") == 0);
+  Assert_String_Equals(representation, "4");
 
   representation = int_to_cstring(5);
-  Assert(strcmp(representation, "5") == 0);
+  Assert_String_Equals(representation, "5");
 
   representation = int_to_cstring(6);
-  Assert(strcmp(representation, "6") == 0);
+  Assert_String_Equals(representation, "6");
 
   representation = int_to_cstring(7);
-  Assert(strcmp(representation, "7") == 0);
+  Assert_String_Equals(representation, "7");
 
   representation = int_to_cstring(8);
-  Assert(strcmp(representation, "8") == 0);
+  Assert_String_Equals(representation, "8");
 
   representation = int_to_cstring(9);
-  Assert(strcmp(representation, "9") == 0);
+  Assert_String_Equals(representation, "9");
 
   representation = int_to_cstring(10);
-  Assert(strcmp(representation, "10") == 0);
+  Assert_String_Equals(representation, "10");
 
   representation = int_to_cstring(12);
-  Assert(strcmp(representation, "12") == 0);
+  Assert_String_Equals(representation, "12");
 
   representation = int_to_cstring(23);
-  Assert(strcmp(representation, "23") == 0);
+  Assert_String_Equals(representation, "23");
 
   representation = int_to_cstring(154);
-  Assert(strcmp(representation, "154") == 0);
+  Assert_String_Equals(representation, "154");
 
   representation = int_to_cstring(20056);
-  Assert(strcmp(representation, "20056") == 0);
+  Assert_String_Equals(representation, "20056");
 
   // @note Em C não existe um zero negativo inteiro, porém o operador '-' pode ser especificado a frente de um zero
   // e não produzira efeito nenhum. Esse teste abaixo fica mais como uma curiosidade do que como um teste prático.
   representation = int_to_cstring(-0); 
-  Assert(strcmp(representation, "0") == 0);
+  Assert_String_Equals(representation, "0");
 
   representation = int_to_cstring(-1);
-  Assert(strcmp(representation, "-1") == 0);
+  Assert_String_Equals(representation, "-1");
 
   representation = int_to_cstring(-2);
-  Assert(strcmp(representation, "-2") == 0);
+  Assert_String_Equals(representation, "-2");
 
   representation = int_to_cstring(-3);
-  Assert(strcmp(representation, "-3") == 0);
+  Assert_String_Equals(representation, "-3");
 
   representation = int_to_cstring(-4);
-  Assert(strcmp(representation, "-4") == 0);
+  Assert_String_Equals(representation, "-4");
 
   representation = int_to_cstring(-5);
-  Assert(strcmp(representation, "-5") == 0);
+  Assert_String_Equals(representation, "-5");
 
   representation = int_to_cstring(-6);
-  Assert(strcmp(representation, "-6") == 0);
+  Assert_String_Equals(representation, "-6");
 
   representation = int_to_cstring(-7);
-  Assert(strcmp(representation, "-7") == 0);
+  Assert_String_Equals(representation, "-7");
 
   representation = int_to_cstring(-8);
-  Assert(strcmp(representation, "-8") == 0);
+  Assert_String_Equals(representation, "-8");
 
   representation = int_to_cstring(-9);
-  Assert(strcmp(representation, "-9") == 0);
+  Assert_String_Equals(representation, "-9");
 
   representation = int_to_cstring(-10);
-  Assert(strcmp(representation, "-10") == 0);
+  Assert_String_Equals(representation, "-10");
 
   representation = int_to_cstring(-12);
-  Assert(strcmp(representation, "-12") == 0);
+  Assert_String_Equals(representation, "-12");
 
   representation = int_to_cstring(-23);
-  Assert(strcmp(representation, "-23") == 0);
+  Assert_String_Equals(representation, "-23");
 
   representation = int_to_cstring(-154);
-  Assert(strcmp(representation, "-154") == 0);
+  Assert_String_Equals(representation, "-154");
 
   representation = int_to_cstring(-20056);
-  Assert(strcmp(representation, "-20056") == 0);
+  Assert_String_Equals(representation, "-20056");
 
   representation = int_to_cstring(-20057);
-  Assert(strcmp(representation, "-20057") == 0);
+  Assert_String_Equals(representation, "-20057");
 
   representation = int_to_cstring(-210056);
-  Assert(strcmp(representation, "-210056") == 0);
+  Assert_String_Equals(representation, "-210056");
 }
 
 void test_is_only_spaces_or_empty_01(void)
