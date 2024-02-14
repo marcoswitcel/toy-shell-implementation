bf75ee4ed2158ed5254f472ffe84bc12b18c8019
Joao Marcos <joao.marcos.witcel@gmail.com>
2024-02-13 22:09:10 -0300

    .

--- scripts/tmux-split-screen-debug.sh
+++ scripts/tmux-split-screen-debug.sh
@@ -3,4 +3,10 @@
 # https://unix.stackexchange.com/questions/292137/tmux-script-to-launch-several-commands
 # https://man.openbsd.org/OpenBSD-current/man1/tmux.1#split-window
 
+if ! command -v tmux &> /dev/null
+then
+  echo "Este script depende da ferramenta/programa 'tmux'. Favor realise a instalação."
+  exit 1
+fi
+
 tmux new-session  'make run' \; split-window -h 'tail -f ./bin/debug_file.log' \; select-pane -L
